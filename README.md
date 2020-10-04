67264273b59b2bbd4b4aaaf8b5b34a0152d0b913
Harvey <harveyjwilliams@aol.co.uk>
2020-10-04 23:26:26 +0200

    Allowed startpages to be built with ejs that's rendered on the backend.

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 ## Requirements
 
-- Node v...
+- Node v8.11.1 or higher
+- NPM v5.6.0 or higher
 
 ## Getting started
 
 1. `npm install custom-start-page-server --save`
 2. `node node_modules/custom-start-page-server/index.js`
+
+## Developing this package
+
+1. Run `npm link` in the root of this directory
+2. Go to a startpage and run `npm link custom-start-page-server`
+3. Run `nodemon ../node_modules/custom-start-page-server/ --watch ../ --watch D:\\Dev\\Sites\\custom-start-page\\custom-start-page-server`
+4. Start developing!
--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const express = require('express')
+const fs = require('fs')
+const ejs = require('ejs');
+
 const app = express()
 const port = 3000
 
-app.use(express.static('.'))
+const routing = () => {
+    const hostUserStartPage = () => {
+        app.use(express.static('.', {
+            index: false,
+        }))
 
-app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/index.html')
-})
+        if (fs.existsSync('index.ejs')) {
+            app.get('/', (req, res) => {
+                const data = {
+                    "links": [
+                        {
+                            "name": "Youtube",
+                            "url": "https://www.youtube.com"
+                        },
+                        {
+                            "name": "Netflix",
+                            "url": "https://www.netflix.com"
+                        },
+                        {
+                            "name": "Spotify",
+                            "url": "https://www.spotify.com"
+                        },
+                        {
+                            "name": "Prime",
+                            "url": "https://www.amazon.de/amazonprime?_encoding=UTF8&%2AVersion%2A=1&%2Aentries%2A=0"
+                        }
+                    ]
+                }
 
-console.log(__dirname);
+                const template = fs.readFileSync('index.ejs', { encoding: 'utf8', flag: 'r' })
+                const html = ejs.render(template, { data: data })
 
-app.use('/csp', express.static(__dirname + '/csp'))
+                res.send(html)
+            });
+        } else {
+            app.get('/', (req, res) => {
+                res.sendFile(process.cwd() + '/index.html')
+            })
+        }
+    }
 
-app.get('/settings', (req, res) => {
-    res.sendFile(__dirname + '/csp/edit.html')
-})
+    const hostSettings = () => {
+        // Files for the settings page to work.
+        app.use('/csp', express.static(__dirname + '/csp'))
+
+        // Actual settings page that lets the user customisable the page with.
+        app.get('/settings', (req, res) => {
+            res.sendFile(__dirname + '/csp/edit.html')
+        })
+    }
+
+    hostUserStartPage()
+    hostSettings()
+}
+
+routing()
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
